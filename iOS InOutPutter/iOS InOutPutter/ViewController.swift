08774aefb72cc43f598dc2ac910bf26cc94a1048
Nico <nicolaikrueger@gmx.de>
2016-05-21 00:39:31 +0200

    Added UI

--- iOS InOutPutter/iOS InOutPutter/ViewController.swift
+++ iOS InOutPutter/iOS InOutPutter/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var lbl_main: UILabel!
+    
+    //@IBAction func btn_main(sender: UIButton) {
+    //    lbl_main.text = "Hello World!"
+    //}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lbl_main.text = "Hello World!"
     }
 
     override func didReceiveMemoryWarning() {
