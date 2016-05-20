80330abae42d707b463d08df615046c128d38eab
Timo Koller <timo.koller@delivion.de>
2016-05-20 23:34:22 +0200

    Push

---
+++ iOS InOutPutter/iOS InOutPutter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS InOutPutter
+//
+//  Created by Timo Koller on 20/05/16.
+//  Copyright © 2016 Timo Koller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
