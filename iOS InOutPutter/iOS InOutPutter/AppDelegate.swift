5697200f179330946536413c7cbfefe5fd3f6a4d
Timo Koller <timo.koller@delivion.de>
2016-05-21 00:15:54 +0200

    Added function to read line from console. Also provided the classic "Hello World String" and a little var test.

--- iOS InOutPutter/iOS InOutPutter/AppDelegate.swift
+++ iOS InOutPutter/iOS InOutPutter/AppDelegate.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        struct Fahrenheit {
+            var temperature: Double
+            init() {
+                temperature = 32.0
+            }
+        }
+        var f = Fahrenheit()
+        print("The default temperature is \(f.temperature)° Fahrenheit")
+        // Prints "The default temperature is 32.0° Fahrenheit"
+        
+        let response = readLine(stripNewline: true)
+        print(response)
+        
+        
+        let greeting = "Hello World"
+        print(greeting)
         return true
     }
 
@@ -42,5 +57,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
-}
+  }
 
