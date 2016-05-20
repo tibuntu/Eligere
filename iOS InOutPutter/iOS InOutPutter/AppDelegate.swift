fce0dd2e5052b5f709059d3835154debe2c13030
Nico <nicolaikrueger@gmx.de>
2016-05-20 23:51:41 +0200

    Added container Nico

--- iOS InOutPutter/iOS InOutPutter/AppDelegate.swift
+++ iOS InOutPutter/iOS InOutPutter/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //### Nico ###
+        
+        print("HelloWorld")
+        
+        
         return true
     }
 
