b3a508b807d32873ea987c1a8376b1948c0fcd74
Nico <nicolaikrueger@gmx.de>
2016-05-20 22:47:03 +0200

    Added comments

--- HelloWorld/HelloWorld.playground/Contents.swift
+++ HelloWorld/HelloWorld.playground/Contents.swift
@@ -3,10 +3,13 @@
 import UIKit
 
 
-var str2 = "Change Nico"
 
+//Definition Nico variables
+
+var str_nico1 = "Change 1 Nico"
 var str_nico3 = "Change 3 Nico"
 var str_nico2 = "Change 2 Nico"
 
 
+//Definition program
 var str = "Hello, World!!!!!"
