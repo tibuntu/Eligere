60f21d347365118f0ebed820017472de00a05279
Nico <nicolaikrueger@gmx.de>
2016-05-21 11:37:44 +0200

    Added GUI

--- iOS/iOS/ViewController.swift
+++ iOS/iOS/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var displayLabel: UILabel!
 
+    @IBAction func buttonPress(sender: UIButton) {
+        
+        displayLabel.text = "Hello World!"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
