20323511ff1e9688e7ead8a9e202b7819ddea189
Nico <nicolaikrueger@gmx.de>
2016-05-21 19:32:17 +0200

    Added playground

---
+++ NicosPlayground/NicosPlayground.playground/Contents.swift
@@ -0,0 +1,79 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//define string for output
+
+var str_text: String
+
+
+//set output
+
+str_text = "Hello World"
+
+
+//define tuple
+
+let statusMessage = (statusCode: 403, statusText: "error - not found")
+
+
+//print output
+
+print("The first words in swift 2: \(str_text)")
+
+
+//print tuple items
+
+print("status message: \(statusMessage.statusText) - Code \(statusMessage.statusCode)")
+
+
+
+// ######## DOOR ACCESS ########
+
+//define door
+
+var doorState = false
+var doorAttempts = UInt8()
+doorAttempts = 0
+
+
+//defince code
+
+let accessCode = 5806
+var receivedCode:Int
+
+
+//define emergency
+
+let emergencyCall = (emergencyMessage: "access denied. security alarmed", emergencyNumber: 6667)
+var emergencyTrigger = false
+var cameraState = false
+
+
+//enter code
+
+receivedCode = 5807 //<<<<<< RECEIVE USER INPUT
+
+
+//compare code and open door if equal
+
+
+if accessCode == receivedCode {
+    
+    doorState = true
+    print("door open")
+    
+} else {
+    
+    doorAttempts = doorAttempts + 1
+    
+    if doorAttempts >= 3 {
+        
+        emergencyTrigger = true
+        cameraState = true
+        print("alert: \(emergencyCall.Message) - call string(\(emergencyCall.emergencyNumber))")
+        
+    }
+    
+}
