fc83c0e25aefe69b5f76bf29a37574a998641d77
Timo Koller <timo.koller@delivion.de>
2016-05-26 15:25:29 +0200

    Fixed row selection

--- phptry/mysql.php
+++ phptry/mysql.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+/*
 $sql = "CREATE TABLE name (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
   fullname VARCHAR(30) NOT NULL,
@@ -24,14 +24,14 @@ if ($conn->query($sql) === TRUE) {
 } else {
       echo "Error creating table: " . $conn->error;
 }
-
-$sql = "SELECT * FROM name";
+ */
+$sql = "SELECT fullname FROM name";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
        while($row = $result->fetch_assoc()) {
-       echo "Name: " . $row["*"]. "<br>";
+       echo "Name: " . $row["fullname"]. "<br>";
        }
 } else {
        echo "0 results";
