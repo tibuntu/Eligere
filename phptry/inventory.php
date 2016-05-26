ccf29ceaec2f4fa53bae2f720a141fb50042b2fb
Timo Koller <timo.koller@delivion.de>
2016-05-26 16:17:41 +0200

    Created json-based inventory

---
+++ phptry/inventory.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "pnpadmin";
+$password = "PnProot5806";
+$dbname = "inventory_pnp";
+$tablename = "item";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+$sql = "SELECT * FROM item";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+  while($row = $result->fetch_assoc()) {
+  echo "ID: " . $row["id"].  " - Name: " . $row["fullname"]. "<br>";
+  }
+} else {
+  header("Content-Type: application/json");
+  echo json_encode($results);
+ }
+$conn->close();
+?>
