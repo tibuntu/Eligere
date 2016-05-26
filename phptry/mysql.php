6dbd0dede9ccff024a72800f60a99f072ea050ad
Timo Koller <timo.koller@delivion.de>
2016-05-26 15:09:57 +0200

    Added function to display rows from a table in inventory_pnp DB

--- phptry/mysql.php
+++ phptry/mysql.php
@@ -2,14 +2,27 @@
 $servername = "localhost";
 $username = "pnpadmin";
 $password = "PnProot5806";
-#$database = "inventory_pnp";
+$dbname = "inventory_pnp";
+$table = "name";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "SELECT * FROM $table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+       while($row = $result->fetch_assoc()) {
+       echo "all: " . $row["*"]. "<br>";
+       }
+} else {
+       echo "0 results";
+       }
+ $conn->close();
 ?>
