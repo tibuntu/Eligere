4bbf81d17b481959c1d35c9b86b5eea8b41e3a8e
Timo Koller <timo.koller@delivion.de>
2016-05-26 16:32:00 +0200

    Testing other syntax

--- phptry/inventory.php
+++ phptry/inventory.php
@@ -13,15 +13,16 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-  while($row = $result->fetch_assoc()) {
-  echo "ID: " . $row["id"].  " - Name: " . $row["fullname"]. "<br>";
-  }
-} else {
+  while(row = $result->fetch_assoc()) {
   header("Content-Type: application/json");
   echo json_encode(array(
     "ID:" => $row['id'],
     "Name:" => $row['fullname'],
-   ));
+  ));
+} else {
+echo "ID: " . $row["id"].  " - Name: " . $row["fullname"]. "<br>";
+  }
 }
 $conn->close();
 ?>
+  
