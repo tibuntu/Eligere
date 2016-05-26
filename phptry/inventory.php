c59810bc93f727b86415a70f6d91eaa35696c7e8
Timo Koller <timo.koller@delivion.de>
2016-05-26 17:05:14 +0200

    Fix

--- phptry/inventory.php
+++ phptry/inventory.php
@@ -4,9 +4,9 @@ $username = "pnpadmin";
 $password = "PnProot5806";
 $dbname = "inventory_pnp";
 $tablename = "item";
-
+<head>
 header('Content-Type: application/json; charset=utf-8');
-
+</head>
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
