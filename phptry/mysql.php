10bc43bb1639effa1c02bf562d13d956e6a3e4ef
Timo Koller <timo.koller@delivion.de>
2016-05-26 15:22:25 +0200

    Added function to create a table thats needed

--- phptry/mysql.php
+++ phptry/mysql.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "pnpadmin";
 $password = "PnProot5806";
 $dbname = "inventory_pnp";
-$table = "name";
+#$table = "name";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,13 +13,25 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM $table";
+$sql = "CREATE TABLE name (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+  fullname VARCHAR(30) NOT NULL,
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+      echo "Table name created successfully";
+} else {
+      echo "Error creating table: " . $conn->error;
+}
+
+$sql = "SELECT * FROM name";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
        while($row = $result->fetch_assoc()) {
-       echo "all: " . $row["*"]. "<br>";
+       echo "Name: " . $row["*"]. "<br>";
        }
 } else {
        echo "0 results";
