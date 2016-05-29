5def211a662f3e852a69b905f390260685f5610c
Timo Koller <timo.koller@delivion.de>
2016-05-29 11:56:30 +0200

    Added file for image_upload

---
+++ phptry/img_upload.php
@@ -0,0 +1,21 @@
+<?php
+  $host='localhost';
+  $user='pnpadmin';
+  $password='PnProot5806';
+  $dbname='inventory_pnp';
+  $imgData = file_get_contents($filename);
+  $size = getimagesize($filename);
+
+  mysql_connect($host, "$username", "$password");
+  mysql_select_db ("$dbname");
+  $sql = sprintf("INSERT INTO testblob
+   (image_type, image, image_size, image_name)
+   VALUES
+   ('%s', '%s', '%d', '%s')",
+   mysql_real_escape_string($size['mime']),
+   mysql_real_escape_string($imgData),
+   $size[3],
+   mysql_real_escape_string($_FILES['userfile']['name'])
+   );
+mysql_query($sql);  
+?>
