5b9b5dd7561bbcade3107c0792aae04d706a6397
Timo Koller <timo.koller@delivion.de>
2016-06-04 16:23:08 +0200

    Try to fix con again

--- phptry/vars.php
+++ phptry/vars.php
@@ -2,6 +2,6 @@
   $host='localhost';
   $user='pnpadmin';
   $password='PnProot5806';
-  $dbname='inventory_pn';
+  $dbname='inventory_pnp';
   $connection = new mysqli($host, $user, $password, $dbname);
 ?>
