811e061b9e37ed00bf21a8ebb2504f269bc448bb
Timo Koller <timo.koller@delivion.de>
2016-06-04 15:26:25 +0200

    Fixed loop issue

--- phptry/api.php
+++ phptry/api.php
@@ -7,13 +7,15 @@
   $id = $_POST['b'];
   $reg_date = $_POST['c'];
 
-    do {
+    
+  if (($_GET["getpost"]) == "postItem");
+  {
         $dbconnect = @mysql_select_db('inventory_pnp', $connection);
         $query = "INSERT INTO `inventory_pnp`.`item` (`fullname`)
                   VALUES ('$fullname');";
         mysql_query($query, $connection) or die(mysql_error());
-          echo $query;
-      } while (($_GET["getpost"]) == "postItem");
+        echo $query;
+  }
        /*
 else {
 ,'$reg_date','$id'
