4dafaa1e4c1e09488de682b2fd38f4616b127de3
Timo Koller <timo.koller@delivion.de>
2016-06-04 20:58:32 +0200

    Created api structure

--- phptry/getItem.php
+++ phptry/getItem.php
@@ -6,16 +6,15 @@
      $result = $connection->query($sql);
      $results = array ();
      header("Content-Type: application/json; charset=utf-8");
-
      if ($result->num_rows > 0) 
      {
         while($row = $result->fetch_assoc()) 
         {
           $results[] = $row;
         }
-       echo json_encode($results, JSON_PRETTY_PRINT);
+     echo json_encode($results, JSON_PRETTY_PRINT);
      }
-     $conn->close();
+     $connection->close();
    }
 ?>
   
