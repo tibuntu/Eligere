a91857b167993dd27423456c5c9c200e2fdfbdb7
Timo Koller <timo.koller@delivion.de>
2016-06-04 15:37:03 +0200

    Added function to get inventory

--- phptry/api.php
+++ phptry/api.php
@@ -7,14 +7,29 @@
   $id = $_POST['b'];
   $reg_date = $_POST['c'];
 
-    
-  if (($_GET["getpost"]) == "postItem");
+
+  switch (($_GET["getpost"]))
   {
+  case "postItem":
         $dbconnect = @mysql_select_db('inventory_pnp', $connection);
         $query = "INSERT INTO `inventory_pnp`.`item` (`fullname`)
                   VALUES ('$fullname');";
         mysql_query($query, $connection) or die(mysql_error());
         echo $query;
+        break;
+  case "getItem":
+        $sql = "SELECT * FROM item";
+        $result = $conn->query($sql);
+        $results = array ();
+        header("Content-Type: application/json; charset=utf-8");
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+        $results[] = $row;
+        }
+        echo json_encode($results, JSON_PRETTY_PRINT);
+        }
+      $conn->close();
   }
        /*
 else {
