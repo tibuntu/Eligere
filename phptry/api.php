e32b98a834340fcf6eee1d415d4e6dcd056a7ebe
Timo Koller <timo.koller@delivion.de>
2016-06-04 15:18:29 +0200

    Fixed code

--- phptry/api.php
+++ phptry/api.php
@@ -9,15 +9,15 @@
 
     do {
         $dbconnect = @mysql_select_db('inventory_pnp', $connection);
-        $query = "INSERT INTO `inventory_pnp`.`item` (`fullname`, `id`, `reg_date`)
-                  VALUES ('$fullname','$reg_date','$id');";
+        $query = "INSERT INTO `inventory_pnp`.`item` (`fullname`)
+                  VALUES ('$fullname');";
         mysql_query($query, $connection) or die(mysql_error());
-          echo 'Successfully added.';
           echo $query;
-      } while (($_GET["getpost"];) == "getsetdata");
+      } while (($_GET["getpost"];) == "postItem");
        /*
-        else {
-
+else {
+,'$reg_date','$id'
+, `id`, `reg_date`
              }
        */
 ?>
