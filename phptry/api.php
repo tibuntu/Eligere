a0ca2604a273d8d4753a02db6ae2fa3b9d47dd1e
Timo Koller <timo.koller@delivion.de>
2016-06-04 23:57:31 +0200

    Added new functions to get and create Sets

--- phptry/api.php
+++ phptry/api.php
@@ -2,14 +2,20 @@
   include 'vars.php';
   include 'getItem.php';
   include 'postItem.php';
-  
-  switch (($method["getpost"])) 
-  {
-  case "postItem":
-    postItem();
-    break;
-  case "getItem":
+  include 'postSet.php';
+  include 'getSet.php';
+  $uri = $_SERVER['QUERY_STRING'];
+
+  if (strpos($uri, 'getItem') !== false) {
     getItem();
-    break;          
-  }
+   }
+  elseif (strpos($uri, 'postItem') !== false) {
+    postItem();
+   }
+  elseif (strpos($uri, 'getSet') !== false) {
+    getSet();
+   }
+  elseif (strpos($uri, 'postSet') !== false) {
+    postSet();
+   }
 ?>
