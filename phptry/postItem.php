2cbb9a51c0e8023542f2ecafcd2be6b957af8fc7
Timo Koller <timo.koller@delivion.de>
2016-06-04 16:51:44 +0200

    Fixed post

--- phptry/postItem.php
+++ phptry/postItem.php
@@ -5,10 +5,9 @@
       $fullname = $_POST['a'];
       $id = $_POST['b'];
       $reg_date = $_POST['c'];
-      $dbconnect = @mysql_select_db('inventory_pnp', $connection);
 
       $query = "INSERT INTO `inventory_pnp`.`item` (`fullname`)
                 VALUES ('$fullname');";
-      mysql_query($query, $connection) or die(mysql_error());
+      mysqli_query($query, $connection) or die(mysqli_error());
     }
 ?>
